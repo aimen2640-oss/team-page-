7913581ab40f5f8ca2c818f4ac5ab4ec8a11792d
aimen2640-oss <uzairahmed2640@gmail.com>
2026-03-28 20:12:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple click alert (bonus interaction)
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        alert("Team member profile clicked!");
+    });
+});
